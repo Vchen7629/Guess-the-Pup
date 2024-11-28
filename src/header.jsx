3004a30de9f6f1df710d10b7feae2480ab9cf485
Vincent Chen <vincentkuangchen@gmail.com>
2024-11-27 20:24:52 -0800

    Updates

---
+++ src/header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="flex h-16  w-full bg-blue-500 text-2xl items-center justify-center font-bold text-white">
+            Guess The Pup
+        </div>
+    )
+}
+
+export default Header
